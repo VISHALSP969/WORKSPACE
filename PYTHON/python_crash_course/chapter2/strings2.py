67cf951a0f8d587fc5178ab4d9971fa22421c28e
VISHALSP969 <vishalsp969@gmail.com>
2025-09-14 13:26:21 +0530

    Changing Case in a String with Methods

---
+++ PYTHON/python_crash_course/chapter2/strings2.py
@@ -0,0 +1,7 @@
+name="ada lovelace"
+print(name.title())     # changes each word to title case
+print(name.capitalize())    # only capitalize first letter
+
+name="Ada Lovelace"
+print(name.upper())     # change each letter to uppercase
+print(name.lower())     # change each letter to lower case
