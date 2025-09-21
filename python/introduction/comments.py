f94e6944eb7b3101567383bdcd3a70bd58483413
VISHALSP969 <vishalsp969@gmail.com>
2025-09-22 01:01:42 +0530

    Comments in Python

---
+++ python/introduction/comments.py
@@ -0,0 +1,14 @@
+# This is single-line comment
+print("Hello, World!")
+
+'''
+This is multi-line comment
+using triple single quotes.
+It spans across multiple lines.
+'''
+"""
+This is also a multi-line comment
+using triple double quotes.
+Both work the same way.
+"""
+print("Python Comments Example")
