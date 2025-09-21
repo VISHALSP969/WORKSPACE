11d642c1c49b874e2477c427c818eca413ac92a5
VISHALSP969 <vishalsp969@gmail.com>
2025-09-22 02:23:35 +0530

    type() function

---
+++ python/variables_constants/type_function.py
@@ -0,0 +1,13 @@
+x=5
+print(type(x))
+
+print(type(5))
+print(type(3.14))
+print(type("Hello"))
+print(type([1,2,3]))
+print(type((1,2,3)))
+print(type({1:"a"}))
+print(type({1,2,3}))
+print(type(2+3j))
+print(type(True))
+print(type(None))
