9badbed69e13cde9f5225191ae68b000c2d5f065
VISHALSP969 <vishalsp969@gmail.com>
2025-09-22 14:39:30 +0530

    Import constant from module

--- python/variables_constants/exercise7.py
+++ python/variables_constants/exercise7.py
@@ -1,7 +1,7 @@
 import constants
 
-print("VAlue of PI :",constants.PI)     # prints 3.14159
+print("Value of PI :",constants.PI)     # prints 3.14159
 
 # Try to reassign
 constants.PI=4
-print("VAlue of PI :",constants.PI)     # prints 4
+print("Value of PI :",constants.PI)     # prints 4
