b2f025369f56bc7e67d4dc1f85b47645568db315
VISHALSP969 <vishalsp969@gmail.com>
2025-09-22 21:59:29 +0530

    Store and print student information

---
+++ python/variables_constants/exercise8.py
@@ -0,0 +1,13 @@
+# Store and print student information
+
+# Store student information
+name="John"
+age=22
+grade="A"
+city="Delhi"
+
+# Print details
+print("Student Name :",name)
+print("Age :",age)
+print("Grade:",grade)
+print("City :",city)
