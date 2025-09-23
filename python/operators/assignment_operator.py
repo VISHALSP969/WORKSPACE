1ce50230a94a0ba61fd385150f017f88eb860520
VISHALSP969 <vishalsp969@gmail.com>
2025-09-24 01:52:22 +0530

    Assignment operator

---
+++ python/operators/assignment_operator.py
@@ -0,0 +1,34 @@
+# Simple assignment
+x=10
+print
+a,b=1,2
+print(a,b)
+x=y=0
+print(x,y)
+
+# Augmented assignment
+x=5
+print(x)
+x+=5
+print(x)
+x*=3
+print(x)
+x/=5
+print(x)
+x**=2
+print(x)
+x%=10
+print(x)
+x//=5
+print(x)
+
+x=2
+x<<=4
+print(x)
+x>>=2
+print(x)
+
+t=(1,2)
+print(t)
+t+=(3,)
+print(t)
