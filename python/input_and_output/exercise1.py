b909001c8029fdcd72daeb17a50b9f5461723671
VISHALSP969 <vishalsp969@gmail.com>
2025-09-24 18:30:32 +0530

    Mathematical operations on inputs

---
+++ python/input_and_output/exercise1.py
@@ -0,0 +1,10 @@
+# Program to perform addition,subtraction,multiplication and division
+
+# Take two numbers as input(in one line)
+a,b=map(int,input("Enter two numbers : ").split())
+
+# Perform operations
+print("Addition :",a+b)
+print("Subtraction :",a-b)
+print("Multiplcation :",a*b)
+print("Division :",a/b)
