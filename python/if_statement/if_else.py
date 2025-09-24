e1d64de914f1912f333c2b490596a48536c6df77
VISHALSP969 <vishalsp969@gmail.com>
2025-09-24 22:58:19 +0530

    if-else

---
+++ python/if_statement/if_else.py
@@ -0,0 +1,17 @@
+num=int(input("Enter a number : "))
+if num%2==0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
+
+age=int(input("Enter your age : "))
+if age>=18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+password=input("Enter password : ")
+if password=="python123":
+    print("Access granted")
+else:
+    print("Access denied")
