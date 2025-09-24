c2b331b953b07e90d54c4d95442d12a094371e76
VISHALSP969 <vishalsp969@gmail.com>
2025-09-24 15:04:26 +0530

    Operator precedence

---
+++ python/operators/operator_precedence.py
@@ -0,0 +1,22 @@
+r=10+5*2
+print(r)
+r=(10+5)*2
+print(r)
+r=2**3**2
+print(r)
+r=-2**2
+print(r)
+print(3+2>4*1)
+print(True or False and False)
+print(5&3==1)
+
+print(100/10*2)
+print(2**3**2)
+
+x=(10+3)*(2**3)
+print(x)
+
+print(10-2*3)
+print(4+3**2)
+print((5>3) and (2<1) or True)
+print(2+3*4**2)
