d837a6accda14a0cddf07b027bb43f8c15fc9cbf
VISHALSP969 <vishalsp969@gmail.com>
2025-09-24 18:49:07 +0530

    Taking different types

---
+++ python/input_and_output/exercise2.py
@@ -0,0 +1,8 @@
+city=input("Enter your city :")
+print("You live in",city)
+
+price=float(input("Enter the price of an item: "))
+print("Price entered :",price)
+
+x,y,z=map(int,input("Enter three numbers : ").split())
+print("Sum =",x+y+z)
