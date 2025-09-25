32a08dc3a6382cd36f7757e98629995962f835c1
VISHALSP969 <vishalsp969@gmail.com>
2025-09-26 05:10:12 +0530

    Even and Odd numbers

---
+++ python/for_loop/even_odd.py
@@ -0,0 +1,13 @@
+for i in range(2,21,2):
+    print(i,end=" ")
+print()
+
+for i in range(1,20,2):
+    print(i,end=" ")
+print()
+
+for i in range(1,21):
+    if i%2==0:
+        print(f"{i} is Even")
+    else:
+        print(f"{i} is Odd")
