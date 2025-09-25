05c247f27e3e8b564b92986481ab9ff2f853fc33
VISHALSP969 <vishalsp969@gmail.com>
2025-09-26 00:10:32 +0530

    Print numbers

---
+++ python/for_loop/print_numbers.py
@@ -0,0 +1,16 @@
+for i in range(11):     #  start=,stop=11,step=1
+    print(i,end="  ")
+print()
+
+for i in range(1,11):
+    print(i,end="  ")
+print()
+
+for i in range(1,11,2):
+    print(i,end="  ")
+print()
+
+# reverse loop
+for i in range(10,0,-1):
+    print(i,end="  ")
+print()
