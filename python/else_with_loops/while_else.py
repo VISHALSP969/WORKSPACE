59e81ade8dae896d9edc86d57f51828d1999be02
VISHALSP969 <vishalsp969@gmail.com>
2025-09-26 08:08:05 +0530

    while_else

---
+++ python/else_with_loops/while_else.py
@@ -0,0 +1,15 @@
+i=1
+while i<=5:
+    print(i)
+    i+=1
+else:
+    print("While loop completed successfully")
+
+i=1
+while i<=5:
+    if i==3:
+        break
+    print(i)
+    i+=1
+else:
+    print("While loop completed successfully")
