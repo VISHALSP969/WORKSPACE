af2e00391e0e3ac499d0be66bae08482d7c242bb
VISHALSP969 <vishalsp969@gmail.com>
2025-09-26 13:29:18 +0530

    Slicing

---
+++ python/strings/slicing.py
@@ -0,0 +1,18 @@
+s="Hello, World!"
+
+# Indices :  0 1 2 3 4 5 6 7 8 9 10 11 12
+# characters H e l l o ,   W o r  l  d  !
+
+print(s)
+print(type(s))
+print(len(s))
+print()
+
+print(s[0:5])
+print(s[:5])
+print(s[7:])
+print(s[::2])
+print(s[1:8:2])
+
+print(s[-6:])
+print(s[:-7])
