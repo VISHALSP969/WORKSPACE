8d139f4d1aea2fe0c0e158ba8baaa6c42837e9cc
VISHALSP969 <vishalsp969@gmail.com>
2025-09-26 12:53:49 +0530

    String Indexing

---
+++ python/strings/string_indexing.py
@@ -0,0 +1,11 @@
+s="Hello, World!"
+# Indices :  0 1 2 3 4 5 6 7 8 9 10 11 12
+# characters H e l l o ,   W o r  l  d  !
+print(s)
+print(len(s))
+print(s[0])
+print(s[7])
+print(s[len(s)-1])
+print(s[-1])
+print(s[-6])
+print(s[-len(s)])
