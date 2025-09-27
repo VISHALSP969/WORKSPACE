b748fd678523a6507397ff6646307cb86e15e88f
VISHALSP969 <vishalsp969@gmail.com>
2025-09-27 18:11:33 +0530

    Sort & find min and max value

---
+++ python/lists/sorting.py
@@ -0,0 +1,11 @@
+numbers=[45,2,89,33,67,22]
+
+print(numbers)
+
+# sort numbers
+numbers.sort()
+print(numbers)
+
+# find max and min
+print("Maximum :",max(numbers))
+print("Minimum :",min(numbers))
