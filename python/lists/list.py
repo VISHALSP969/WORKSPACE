88b88083a34505ee2dd5cd5e4441fab4191f94e4
VISHALSP969 <vishalsp969@gmail.com>
2025-09-27 12:52:01 +0530

    lists

---
+++ python/lists/list.py
@@ -0,0 +1,15 @@
+# empty list
+empty_list=[]
+print(empty_list)
+
+# list of integers
+numbers=[10,20,30,40]
+print(numbers)
+
+# mixed data types
+mixed=[25,"Python",3.14,True]
+print(mixed)
+
+# nested list
+nested=[[1,2,3],["a","b","c"]]
+print(nested)
