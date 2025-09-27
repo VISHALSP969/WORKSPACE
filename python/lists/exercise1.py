177829f25deb204023599c4fa504d14010439d2d
VISHALSP969 <vishalsp969@gmail.com>
2025-09-27 14:23:10 +0530

    First,Last and Middle element

---
+++ python/lists/exercise1.py
@@ -0,0 +1,8 @@
+numbers=[5,10,15,20,25,30,35,40,45,50]
+
+print("First element :",numbers[0])
+
+print("Last element :",numbers[-1])
+
+middle_index=len(numbers)//2
+print("Middle element :",numbers[middle_index])
