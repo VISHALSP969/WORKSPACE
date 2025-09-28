7b00ed3ac6fbf7c93aed9312c03924e359fa538e
VISHALSP969 <vishalsp969@gmail.com>
2025-09-28 18:33:21 +0530

    Dictionaries

---
+++ python/dictionaries/dictionary.py
@@ -0,0 +1,16 @@
+# empty dictionary
+empty_dict={}
+print(empty_dict)
+print(type(empty_dict))
+
+# dictionary with student details
+student={
+    "name":"Alice",
+    "age":21,
+    "city":"New York"
+}
+print(student)
+
+# using dict() constructor
+person=dict(name="Bob",age=25,city="London")
+print(person)
