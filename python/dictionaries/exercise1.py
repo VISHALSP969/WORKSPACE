f2aadd177fd99452577d88f32abe3946ff789633
VISHALSP969 <vishalsp969@gmail.com>
2025-09-29 00:51:34 +0530

    Print dictionary data

---
+++ python/dictionaries/exercise1.py
@@ -0,0 +1,12 @@
+# creating student dictionary
+student={
+    "name":"John",
+    "age":22,
+    "marks":85
+}
+
+# printing dictionary
+print("Student Details : ")
+print(f"Name : {student["name"]}")
+print(f"Age : {student["age"]}")
+print(f"Marks : {student["marks"]}")
