e9d7f3e2d897a151caa54090098bfae5f96a2e1a
VISHALSP969 <vishalsp969@gmail.com>
2025-09-28 14:52:34 +0530

    Set Operations

---
+++ python/sets/set_operations.py
@@ -0,0 +1,19 @@
+A={1,2,3}
+B={3,4,5}
+# union
+print(A|B)
+print(A.union(B))
+
+# intersection
+print(A&B)
+print(A.intersection(B))
+
+# difference
+print(A-B)
+print(A.difference(B))
+print(B-A)
+print(B.difference(A))
+
+# symmetric_difference
+print(A^B)
+print(A.symmetric_difference(B))
