e8a000c3906af7699e5a2fa01659bb976f204b80
VISHALSP969 <vishalsp969@gmail.com>
2025-09-28 18:45:48 +0530

    Accessing dictionary values

---
+++ python/dictionaries/access_values.py
@@ -0,0 +1,15 @@
+student={
+    "name":"Alice",
+    "age":21,
+    "city":"New York"
+}
+
+print(student["name"])
+print(student["age"])
+print(student["city"])
+print()
+
+person=dict(name="Bob",age=25,city="London")
+print(person.get("name"))
+print(person.get("age"))
+print(person.get("city"))
