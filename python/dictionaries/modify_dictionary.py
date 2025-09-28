58d8fa0ea0fbfb2b827422c663c87c5a0652b02f
VISHALSP969 <vishalsp969@gmail.com>
2025-09-28 19:55:38 +0530

    Modifying dictionary

---
+++ python/dictionaries/modify_dictionary.py
@@ -0,0 +1,14 @@
+student={
+    "name":"Alice",
+    "age":21,
+    "city":"New York"
+}
+print(student)
+
+# update existing key
+student["age"]=22
+print(student)
+
+# add a new key-value pair
+student["grade"]="A"
+print(student)
