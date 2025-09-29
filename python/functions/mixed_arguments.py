06f376453513cfde64359a7e244b66c37edcafd6
VISHALSP969 <vishalsp969@gmail.com>
2025-09-29 23:09:50 +0530

    Mixed Arguments

---
+++ python/functions/mixed_arguments.py
@@ -0,0 +1,6 @@
+def introduce(name,age=18,city='Unknown'):
+    print(f"my name is {name}, I am {age} years old, from {city}.")
+
+introduce("Charlie")
+introduce("David",22)
+introduce("Emma",city="London")
